a4188dd33d82b5436dbb595979463fd22469895d
akanj <me@akanj.com>
2017-07-30 02:58:09 -0300

    Minor changes

--- Papyrus/AppDelegate.swift
+++ Papyrus/AppDelegate.swift
@@ -16,5 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        MCManager.shared.foundPeers.removeAll()
+    }
 }
 
